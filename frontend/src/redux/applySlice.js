d456c2ab9974c5037a27da098de8f9c5de7d6a9d
Prakash R <blogprakas@gmail.com>
2025-12-07 23:34:39 +0530

    Update API_BASE environment variable reference

--- frontend/src/redux/applySlice.js
+++ frontend/src/redux/applySlice.js
@@ -1,10 +1,10 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_BASE = import.meta.env.REACT_APP_API_BASE || "";
+const API_BASE = import.meta.env.VITE_API_BASE || "";
 const API_URL = `${API_BASE.replace(/\/$/, "")}/apply`; 
 
-console.log("API_BASE:", import.meta.env.REACT_APP_API_BASE);
+console.log("API_BASE:", import.meta.env.VITE_API_BASE);
 console.log("API_URL:", API_URL);
 
 export const submitApplication = createAsyncThunk(
